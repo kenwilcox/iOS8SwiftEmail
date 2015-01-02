858a3426d70d88ad0ebcdd8bb13997687da2e394
kenwilcox <wilcoxk@gmail.com>
2015-01-02 14:50:25 -0700

    added action and outlets

--- iOS8SwiftEmail/ViewController.swift
+++ iOS8SwiftEmail/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var subject: UITextField!
+  @IBOutlet var body: UITextView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,8 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func sendMail(sender: AnyObject) {
+  }
 
 }
 
